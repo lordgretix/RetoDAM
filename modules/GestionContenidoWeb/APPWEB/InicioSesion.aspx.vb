c72fa866c056e5c01d855aafa883454fd7ea71b8
katty <katty944@gmail.com>
2018-12-17 00:13:29 +0100

    InicioSesion

---
+++ modules/GestionContenidoWeb/APPWEB/InicioSesion.aspx.vb
@@ -0,0 +1,15 @@
+﻿
+Partial Class InicioSesion
+    Inherits System.Web.UI.Page
+
+    Protected Sub btnInicio_Click(sender As Object, e As EventArgs) Handles btnInicio.Click
+        If Me.txtNombre.Text = "A" Then
+            Response.Redirect("Default.aspx")
+        Else
+            Me.lblMensaje.Visible = True
+            Me.lblMensaje.Text = "Usuario no identificado"
+            Me.txtNombre.Text = ""
+            Me.txtPassword.Text = ""
+        End If
+    End Sub
+End Class
