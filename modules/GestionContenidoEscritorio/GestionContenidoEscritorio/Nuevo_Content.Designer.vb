00e56f93d2abf773350b64996686185057501a59
Sammuz <>
2018-12-19 14:17:39 +0100

    añadir content

--- modules/GestionContenidoEscritorio/GestionContenidoEscritorio/Nuevo_Content.Designer.vb
+++ modules/GestionContenidoEscritorio/GestionContenidoEscritorio/Nuevo_Content.Designer.vb
@@ -29,66 +29,350 @@ Partial Class Nuevo_Content
         Me.Text_web = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Check_certificado = New System.Windows.Forms.CheckBox()
+        Me.Check_club = New System.Windows.Forms.CheckBox()
+        Me.Check_restaurante = New System.Windows.Forms.CheckBox()
+        Me.Check_tienda = New System.Windows.Forms.CheckBox()
+        Me.Check_caravana = New System.Windows.Forms.CheckBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Text_capacity = New System.Windows.Forms.TextBox()
+        Me.Text_CP = New System.Windows.Forms.TextBox()
+        Me.Text_lat = New System.Windows.Forms.TextBox()
+        Me.Text_muni = New System.Windows.Forms.TextBox()
+        Me.Text_terri = New System.Windows.Forms.TextBox()
+        Me.Text_firma = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Btn_limpiar = New System.Windows.Forms.Button()
+        Me.Btn_add = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Text_nombre
         '
-        Me.Text_nombre.Location = New System.Drawing.Point(266, 48)
+        Me.Text_nombre.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Text_nombre.Location = New System.Drawing.Point(164, 48)
         Me.Text_nombre.Name = "Text_nombre"
-        Me.Text_nombre.Size = New System.Drawing.Size(100, 20)
+        Me.Text_nombre.Size = New System.Drawing.Size(202, 24)
         Me.Text_nombre.TabIndex = 0
         '
         'Text_tel
         '
-        Me.Text_tel.Location = New System.Drawing.Point(266, 89)
+        Me.Text_tel.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Text_tel.Location = New System.Drawing.Point(164, 89)
         Me.Text_tel.Name = "Text_tel"
-        Me.Text_tel.Size = New System.Drawing.Size(100, 20)
+        Me.Text_tel.Size = New System.Drawing.Size(202, 24)
         Me.Text_tel.TabIndex = 1
         '
         'Text_direccion
         '
-        Me.Text_direccion.Location = New System.Drawing.Point(266, 129)
+        Me.Text_direccion.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Text_direccion.Location = New System.Drawing.Point(164, 129)
         Me.Text_direccion.Name = "Text_direccion"
-        Me.Text_direccion.Size = New System.Drawing.Size(100, 20)
+        Me.Text_direccion.Size = New System.Drawing.Size(202, 24)
         Me.Text_direccion.TabIndex = 2
         '
         'Text_email
         '
-        Me.Text_email.Location = New System.Drawing.Point(266, 171)
+        Me.Text_email.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Text_email.Location = New System.Drawing.Point(164, 171)
         Me.Text_email.Name = "Text_email"
-        Me.Text_email.Size = New System.Drawing.Size(100, 20)
+        Me.Text_email.Size = New System.Drawing.Size(202, 24)
         Me.Text_email.TabIndex = 3
         '
         'Text_web
         '
-        Me.Text_web.Location = New System.Drawing.Point(266, 210)
+        Me.Text_web.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Text_web.Location = New System.Drawing.Point(164, 210)
         Me.Text_web.Name = "Text_web"
-        Me.Text_web.Size = New System.Drawing.Size(100, 20)
+        Me.Text_web.Size = New System.Drawing.Size(202, 24)
         Me.Text_web.TabIndex = 4
         '
         'Label1
         '
         Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.Location = New System.Drawing.Point(55, 55)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(47, 13)
+        Me.Label1.Size = New System.Drawing.Size(66, 18)
         Me.Label1.TabIndex = 5
         Me.Label1.Text = "Nombre:"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label2.Location = New System.Drawing.Point(55, 96)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(52, 13)
+        Me.Label2.Size = New System.Drawing.Size(70, 18)
         Me.Label2.TabIndex = 6
         Me.Label2.Text = "Telefono:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(55, 136)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(75, 18)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Direccion:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(55, 178)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(49, 18)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Email:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(55, 217)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(43, 18)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Web:"
+        '
+        'Check_certificado
+        '
+        Me.Check_certificado.AutoSize = True
+        Me.Check_certificado.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Check_certificado.Location = New System.Drawing.Point(64, 251)
+        Me.Check_certificado.Name = "Check_certificado"
+        Me.Check_certificado.Size = New System.Drawing.Size(98, 22)
+        Me.Check_certificado.TabIndex = 10
+        Me.Check_certificado.Text = "Certificado"
+        Me.Check_certificado.UseVisualStyleBackColor = True
+        '
+        'Check_club
+        '
+        Me.Check_club.AutoSize = True
+        Me.Check_club.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Check_club.Location = New System.Drawing.Point(194, 251)
+        Me.Check_club.Name = "Check_club"
+        Me.Check_club.Size = New System.Drawing.Size(57, 22)
+        Me.Check_club.TabIndex = 11
+        Me.Check_club.Text = "Club"
+        Me.Check_club.UseVisualStyleBackColor = True
+        '
+        'Check_restaurante
+        '
+        Me.Check_restaurante.AutoSize = True
+        Me.Check_restaurante.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Check_restaurante.Location = New System.Drawing.Point(288, 251)
+        Me.Check_restaurante.Name = "Check_restaurante"
+        Me.Check_restaurante.Size = New System.Drawing.Size(107, 22)
+        Me.Check_restaurante.TabIndex = 12
+        Me.Check_restaurante.Text = "Restaurante"
+        Me.Check_restaurante.UseVisualStyleBackColor = True
+        '
+        'Check_tienda
+        '
+        Me.Check_tienda.AutoSize = True
+        Me.Check_tienda.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Check_tienda.Location = New System.Drawing.Point(64, 284)
+        Me.Check_tienda.Name = "Check_tienda"
+        Me.Check_tienda.Size = New System.Drawing.Size(71, 22)
+        Me.Check_tienda.TabIndex = 13
+        Me.Check_tienda.Text = "Tienda"
+        Me.Check_tienda.UseVisualStyleBackColor = True
+        '
+        'Check_caravana
+        '
+        Me.Check_caravana.AutoSize = True
+        Me.Check_caravana.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Check_caravana.Location = New System.Drawing.Point(194, 284)
+        Me.Check_caravana.Name = "Check_caravana"
+        Me.Check_caravana.Size = New System.Drawing.Size(117, 22)
+        Me.Check_caravana.TabIndex = 14
+        Me.Check_caravana.Text = "Autocaravana"
+        Me.Check_caravana.UseVisualStyleBackColor = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(55, 322)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(82, 18)
+        Me.Label6.TabIndex = 15
+        Me.Label6.Text = "Capacidad:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(55, 356)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(104, 18)
+        Me.Label7.TabIndex = 16
+        Me.Label7.Text = "Codigo postal:"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(55, 389)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(65, 18)
+        Me.Label8.TabIndex = 17
+        Me.Label8.Text = "LatLong:"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(55, 425)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(75, 18)
+        Me.Label9.TabIndex = 18
+        Me.Label9.Text = "Municipio:"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(55, 454)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(72, 18)
+        Me.Label10.TabIndex = 19
+        Me.Label10.Text = "Territorio:"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.Location = New System.Drawing.Point(55, 485)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(50, 18)
+        Me.Label11.TabIndex = 20
+        Me.Label11.Text = "Firma:"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label12.Location = New System.Drawing.Point(55, 511)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(45, 18)
+        Me.Label12.TabIndex = 21
+        Me.Label12.Text = "Json:"
+        '
+        'Text_capacity
+        '
+        Me.Text_capacity.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Text_capacity.Location = New System.Drawing.Point(164, 314)
+        Me.Text_capacity.Name = "Text_capacity"
+        Me.Text_capacity.Size = New System.Drawing.Size(202, 24)
+        Me.Text_capacity.TabIndex = 22
+        '
+        'Text_CP
+        '
+        Me.Text_CP.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Text_CP.Location = New System.Drawing.Point(164, 348)
+        Me.Text_CP.Name = "Text_CP"
+        Me.Text_CP.Size = New System.Drawing.Size(202, 24)
+        Me.Text_CP.TabIndex = 23
+        '
+        'Text_lat
+        '
+        Me.Text_lat.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Text_lat.Location = New System.Drawing.Point(164, 381)
+        Me.Text_lat.Name = "Text_lat"
+        Me.Text_lat.Size = New System.Drawing.Size(202, 24)
+        Me.Text_lat.TabIndex = 24
+        '
+        'Text_muni
+        '
+        Me.Text_muni.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Text_muni.Location = New System.Drawing.Point(164, 417)
+        Me.Text_muni.Name = "Text_muni"
+        Me.Text_muni.Size = New System.Drawing.Size(202, 24)
+        Me.Text_muni.TabIndex = 25
+        '
+        'Text_terri
+        '
+        Me.Text_terri.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Text_terri.Location = New System.Drawing.Point(164, 446)
+        Me.Text_terri.Name = "Text_terri"
+        Me.Text_terri.Size = New System.Drawing.Size(202, 24)
+        Me.Text_terri.TabIndex = 26
+        '
+        'Text_firma
+        '
+        Me.Text_firma.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Text_firma.Location = New System.Drawing.Point(164, 477)
+        Me.Text_firma.Name = "Text_firma"
+        Me.Text_firma.Size = New System.Drawing.Size(202, 24)
+        Me.Text_firma.TabIndex = 27
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(164, 503)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(202, 24)
+        Me.TextBox1.TabIndex = 28
+        '
+        'Btn_limpiar
+        '
+        Me.Btn_limpiar.Location = New System.Drawing.Point(64, 568)
+        Me.Btn_limpiar.Name = "Btn_limpiar"
+        Me.Btn_limpiar.Size = New System.Drawing.Size(75, 23)
+        Me.Btn_limpiar.TabIndex = 29
+        Me.Btn_limpiar.Text = "Limpiar"
+        Me.Btn_limpiar.UseVisualStyleBackColor = True
+        '
+        'Btn_add
+        '
+        Me.Btn_add.Location = New System.Drawing.Point(269, 568)
+        Me.Btn_add.Name = "Btn_add"
+        Me.Btn_add.Size = New System.Drawing.Size(75, 23)
+        Me.Btn_add.TabIndex = 30
+        Me.Btn_add.Text = "Añadir"
+        Me.Btn_add.UseVisualStyleBackColor = True
         '
         'Nuevo_Content
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(505, 434)
+        Me.ClientSize = New System.Drawing.Size(484, 633)
+        Me.Controls.Add(Me.Btn_add)
+        Me.Controls.Add(Me.Btn_limpiar)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Text_firma)
+        Me.Controls.Add(Me.Text_terri)
+        Me.Controls.Add(Me.Text_muni)
+        Me.Controls.Add(Me.Text_lat)
+        Me.Controls.Add(Me.Text_CP)
+        Me.Controls.Add(Me.Text_capacity)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Check_caravana)
+        Me.Controls.Add(Me.Check_tienda)
+        Me.Controls.Add(Me.Check_restaurante)
+        Me.Controls.Add(Me.Check_club)
+        Me.Controls.Add(Me.Check_certificado)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Text_web)
@@ -109,4 +393,28 @@ Partial Class Nuevo_Content
     Friend WithEvents Text_web As System.Windows.Forms.TextBox
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Check_certificado As System.Windows.Forms.CheckBox
+    Friend WithEvents Check_club As System.Windows.Forms.CheckBox
+    Friend WithEvents Check_restaurante As System.Windows.Forms.CheckBox
+    Friend WithEvents Check_tienda As System.Windows.Forms.CheckBox
+    Friend WithEvents Check_caravana As System.Windows.Forms.CheckBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+    Friend WithEvents Text_capacity As System.Windows.Forms.TextBox
+    Friend WithEvents Text_CP As System.Windows.Forms.TextBox
+    Friend WithEvents Text_lat As System.Windows.Forms.TextBox
+    Friend WithEvents Text_muni As System.Windows.Forms.TextBox
+    Friend WithEvents Text_terri As System.Windows.Forms.TextBox
+    Friend WithEvents Text_firma As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Btn_limpiar As System.Windows.Forms.Button
+    Friend WithEvents Btn_add As System.Windows.Forms.Button
 End Class
